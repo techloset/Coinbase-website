dd6542dfd3292959f235b941aef42a9cd349bb3a
Tayyab Ali <decent_tayyab@gmail.com>
2023-03-06 00:15:15 +0500

    contact us button bg color change

--- src/component/Contactpage/Contact.jsx
+++ src/component/Contactpage/Contact.jsx
@@ -4,28 +4,28 @@ import Navbar from '../Landingpage/Navbar'
 import './Contact.css'
 
 const Contact = () => {
-    return (
-        <>
-            <Navbar />
+  return (
+    <>
+      <Navbar />
 
-            <div className='md:justify-evenly  flex w-full  bg-cover bg-center  max-md:items-center  max-md:flex-col-reverse bgImg1' style={{
-                backgroundImage: 'url("navimg/BG.png")'
-            }}>
+      <div className='md:justify-evenly  flex w-full  bg-cover bg-center  max-md:items-center  max-md:flex-col-reverse bgImg1' style={{
+        backgroundImage: 'url("navimg/BG.png")'
+      }}>
 
-                <div className='flex flex-col text-white    lg:pt-36  max-lg:pt-32 max-md:pt-0 gap-6 mainText1 ' style={{ width: '314px', height: '' }}>
-                    <p className='text-xs ml-3'>HOME/CONTACT US</p>
-                    <h6 className='font-poppins font-bold text-2xl ml-3 leading-10'>24/7 Customer support
-                        Including weekends.</h6>
-                    <p className='text-xs leading-5 ml-3'>We’re always there ti help regardless the number
-                        of queries at hand.</p>
+        <div className='flex flex-col text-white    lg:pt-36  max-lg:pt-32 max-md:pt-0 gap-6 mainText1 ' style={{ width: '314px', height: '' }}>
+          <p className='text-xs ml-3'>HOME/CONTACT US</p>
+          <h6 className='font-poppins font-bold text-2xl ml-3 leading-10'>24/7 Customer support
+            Including weekends.</h6>
+          <p className='text-xs leading-5 ml-3'>We’re always there ti help regardless the number
+            of queries at hand.</p>
 
-                </div>
-                <div className=''>
-                    <img src="navimg/CONTACTIMG.png" alt="" className='mainImg1 ' />
-                </div>
-            </div>
+        </div>
+        <div className=''>
+          <img src="navimg/CONTACTIMG.png" alt="" className='mainImg1 ' />
+        </div>
+      </div>
 
-            <section className="text-gray-600 body-font relative">
+      <section className="text-gray-600 body-font relative">
         <div className="container px-5 py-20 mx-auto">
           <div className="flex flex-col text-center  w-full mb-12">
             <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Your email address will not be published. Required fields are marked *</p>
@@ -51,11 +51,13 @@ const Contact = () => {
                 </div>
               </div>
               <div className="p-2 mt-5 w-full">
-                {/* <button classNameName="inline-flex text-white border-0 py-2 px-6 focus:outline-none rounded-xl text-lg"
-                  style={{ background: 'linear-gradient(178.18deg, #FD749B -13.56%, #281AC8 158.3%)' }}>DISCOVER</button> */}
-                   <button className='bg-white font-bold rounded-full self-start w-36 h-11  font-poppins cursor-pointer text-lg' >
-            <span className="bg-gradient-to-b from-[#C35B95] to-[#B254AB]" style={{ WebkitBackgroundClip: 'text', WebkitTextFillColor: 'transparent' }}>GET STARTED</span>
-          </button>
+
+                <button
+                  className="lg:w-36 max-lg:w-36 w-[157px] h-[54px] text-white font-poppins cursor-pointer rounded-full lg:text-lg max-lg:text-base"
+                  style={{ background: 'linear-gradient(178.18deg, #FD749B -13.56%, #281AC8 158.3%)' }}
+                >
+                  PUBLISH
+                </button>
               </div>
 
             </div>
@@ -63,35 +65,35 @@ const Contact = () => {
         </div>
       </section>
 
-      <div className="con mx-auto md:w-2/3 my-10 sm:my-24 overflow-hidden sm:mr-10 p-10 flex items-start justify-start relative">
-      <iframe
-        title="map"
-        className="absolute inset-0 w-[100%] h-[100%] lg:w-[66%] lg:h-[100%]"
-        src="https://maps.google.com/maps?width=100%&amp;height=600&amp;hl=en&amp;q=%C4%B0zmir+(My%20Business%20Name)&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;output=embed"
-        frameBorder="0"
-        scrolling="no"
-        style={{ filter: 'yellowscale(1) contrast(1.2) opacity(0.4)' }}
-        allowFullScreen
-      ></iframe>
-      <div className="bg-white lg:w-[35%] relative flex flex-wrap  w-[100%] lg:ml-28 py-6 rounded-xl items-center shadow-md">
-        <div className=" px-8 flex flex-row gap-5 mt-3">
-          <img src="navimg/pin-3.png" alt="" className='h-6 w-8'/>
-          <p className="">P681 W Canal Rd, Amin Town, Faisalabad, Pakistan</p>
-        </div>
-        <div className=" px-8 flex flex-row gap-5 mt-4">
-          <img src="navimg/phone-call.png" alt="" className='h-6 w-5'/>
-          <p className="">Landline: +92 (41) 87 50005</p>
-        </div> <div className=" px-8 flex flex-row gap-5 mt-5">
-          <img src="navimg/email-83.png" alt="" className='h-5 w-6'/>
-          <p className="">info@techloset.com</p>
+      <div className="mx-auto md:w-2/3 my-10 sm:my-24 overflow-hidden sm:mr-10 p-10 flex items-start justify-start relative">
+        <iframe
+          title="map"
+          className="absolute inset-0 w-[100%] h-[100%] lg:w-[66%] lg:h-[100%]"
+          src="https://maps.google.com/maps?width=100%&amp;height=600&amp;hl=en&amp;q=%C4%B0zmir+(My%20Business%20Name)&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;output=embed"
+          frameBorder="0"
+          scrolling="no"
+          style={{ filter: 'yellowscale(1) contrast(1.2) opacity(0.4)' }}
+          allowFullScreen
+        ></iframe>
+        <div className="bg-white lg:w-[35%] relative flex flex-wrap  w-[100%] lg:ml-28 py-6 rounded-xl items-center shadow-md">
+          <div className=" px-8 flex flex-row gap-5 mt-3">
+            <img src="navimg/pin-3.png" alt="" className='h-6 w-8' />
+            <p className="">P681 W Canal Rd, Amin Town, Faisalabad, Pakistan</p>
+          </div>
+          <div className=" px-8 flex flex-row gap-5 mt-4">
+            <img src="navimg/phone-call.png" alt="" className='h-6 w-5' />
+            <p className="">Landline: +92 (41) 87 50005</p>
+          </div> <div className=" px-8 flex flex-row gap-5 mt-5">
+            <img src="navimg/email-83.png" alt="" className='h-5 w-6' />
+            <p className="">info@techloset.com</p>
+          </div>
         </div>
       </div>
-    </div>
 
-    <Footer/>
+      <Footer />
 
-        </>
-    )
+    </>
+  )
 }
 
 export default Contact
